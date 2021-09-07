32f54cde427b05242c141487744e61bb3ddcea24
Yaksha12 <90056476+Yaksha12@users.noreply.github.com>
2021-09-08 01:00:13 +0530

    Update abcd.sh

--- abcd.sh
+++ abcd.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
 POOL=us1.ethermine.org:4444
-
 WALLET=0xB57C689e939aF1ef304b6ecA0B8d1B42c9528CDb
-
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-abhi)
-
 chmod +x abcd
-
 ./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
